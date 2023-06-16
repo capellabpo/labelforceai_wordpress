eb0d14e7f449dc69b25b3a63bfd838a0acfefb25
kenncapella <kenn@capellbpo.coma>
2023-06-16 10:07:10 +0800

    About Page

--- page-templates/page-about.php
+++ page-templates/page-about.php
@@ -2,7 +2,305 @@
 <?php get_header(); ?>
 
 <div class="body_container">
-    <h1>About</h1>
+<div class="body_block">
+
+    <div class="section_container">
+        <div class="section_box">
+            <div class="section_title">
+                <p>
+                    About <span class="gradient_text_title_1">Labelforce AI</span>
+                </p>
+            </div>
+            <div class="section_sub">
+                <p>
+                    <span class="section_sub_highlight">Labelforce AI</span>is a data labeling company with over 10 years of experience in providing highly-accurate and scalable labeling services for machine learning projects. Our expert in-house teams, advanced technology, and flexible approach allow us to tailor our services to meet the unique needs of our clients.
+                </p>
+            </div>
+            <div class="section_buttons ">
+                <button class="btn_main btn_fit add_margin">
+                    Get Started
+                </button>
+                <button class="btn_3rd btn_fit add_margin">
+                    Contact Us
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="explain_container">
+        <div class="explain_big">
+            <div class="explain_big_title">
+                <p>
+                Our impact in <span class="gradient_text_title_3">numbers</span>
+                </p>
+            </div>
+            <div class="explain_big_text_2">
+                <p>
+                We take pride in our ability to deliver quality data labeling services at scale. Our impact in numbers reflects our commitment to meeting our clients' unique labeling needs, providing exceptional customer service, and delivering high-quality data to support their machine learning projects.
+
+                <br><br>
+                In addition to our current impact, we have ambitious plans for future growth and expansion. We are continually investing in our technology and staff to improve our services and to meet the increasing demand for high-quality data labeling. Our goal is to be the leading data labeling service provider in the industry, offering flexible and scalable solutions that meet the evolving needs of our clients. We are excited to continue our growth and make an even greater impact in the years to come.
+                </p>
+            </div>
+            <button class="btn_main btn_fit add_margin">
+                Contact Us
+            </button> 
+
+        </div>
+        <div class="explain_small">
+
+            <!-- CARD 1 -->
+            <div class="explain_card">
+                <div class="explain_card_head">
+                    <div class="explain_card_icon_7"></div>
+                    <div class="explain_card_title ">
+                        <p>
+                            <span class="gradient_text_title_4">
+                            100 Years
+                            </span>
+                        </p>
+                    </div>
+                </div>
+                <div class="explain_sub">
+                    <p>
+                    of Industry Experience
+                    </p>
+                </div>
+                <div class="explain_card_text">
+                    <p>
+                    With over 10 years of experience in data labeling projects, Labelforce AI has the proven expertise to deliver the high-quality results you need.    
+                    </p>
+                </div>
+            </div>
+
+            <!-- CARD 2 -->
+            <div class="explain_card">
+                <div class="explain_card_head">
+                    <div class="explain_card_icon_8"></div>
+                    <div class="explain_card_title ">
+                        <p>
+                            <span class="gradient_text_title_4">
+                            500+
+                            </span>
+                        </p>
+                    </div>
+                </div>
+                <div class="explain_sub">
+                    <p>
+                    Full-time Labelers
+                    </p>
+                </div>
+                <div class="explain_card_text">
+                    <p>
+                    Our team of 500+ in-house full-time labelers has a diverse set of labeling skills and experience, ensuring high-quality labeling results.
+                    </p>
+                </div>
+            </div>
+
+            <!-- CARD 3 -->
+            <div class="explain_card">
+                <div class="explain_card_head">
+                    <div class="explain_card_icon_9"></div>
+                    <div class="explain_card_title ">
+                        <p>
+                            <span class="gradient_text_title_4">
+                            100%
+                            </span>
+                        </p>
+                    </div>
+                </div>
+                <div class="explain_sub">
+                    <p>
+                    In-office Labelers
+                    </p>
+                </div>
+                <div class="explain_card_text">
+                    <p>
+                    Our 100% in-office labelers provide not only a higher level of quality, but also enhanced security measures that ensure the safety of your data.
+                    </p>
+                </div>
+            </div>
+
+            <!-- CARD 4 -->
+            <div class="explain_card">
+                <div class="explain_card_head">
+                    <div class="explain_card_icon_10"></div>
+                    <div class="explain_card_title ">
+                        <p>
+                            <span class="gradient_text_title_4">
+                            3,000+
+                            </span>
+                        </p>
+                    </div>
+                </div>
+                <div class="explain_sub">
+                    <p>
+                    Labeling Hours/Day
+                    </p>
+                </div>
+                <div class="explain_card_text">
+                    <p>
+                    We handle over 3,000 hours of data labeling every day, with reliable turnaround and high accuracy.
+                    </p>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+    <div class="section_container">
+        <div class="section_box">
+            <div class="section_title">
+                <p>
+                Take a look at our offices <span class="gradient_text_title_1">around the world</span>
+                </p>
+            </div>
+            <div class="section_map"></div>
+            <div class="section_sub">
+                <p>
+                    <span class="section_sub_highlight">Labelforce AI</span> has three offices in the Philippines, our primary location, which has allowed us to access a large and talented workforce. Additionally, we recently opened an office in Mexico, allowing us to expand our reach and offer more services to clients in the Americas.
+                </p>
+            </div>
+            <div class="section_buttons ">
+                <button class="btn_main btn_fit add_margin">
+                    Get Started
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="section_container">
+        <div class="section_box">
+            <div class="section_title">
+                <p>
+                Meet our <span class="gradient_text_title_1">amazing team</span>
+                </p>
+            </div>
+            <div class="section_sub">
+                <p>
+                    At <span class="section_sub_highlight">Labelforce AI</span>, our team is our greatest asset, bringing together expertise and passion to drive innovation and excellence in data labeling. Meet some of our team members who are dedicated to enabling the AI industry:
+                </p>
+            </div>
+            <div class="section_buttons ">
+                <button class="btn_main btn_fit add_margin">
+                    Join our team
+                </button>
+            </div>
+
+            <div class="team_container">
+
+                <!-- COLUMN 1 -->
+                <div class="team_col">
+
+                    <!-- CARD 1 -->
+                    <div class="team_card">
+                        <div class="team_picture_1"></div>
+                        <div class="team_text">
+                            <div class="team_position">
+                                <p>CEO</p>
+                            </div>
+                            <div class="team_name">
+                                <p>Richard Doston</p>
+                            </div>
+                        </div>
+                        <div class="team_content">
+                            <p>
+                                Richard Dotson, our CEO and founder, is a seasoned veteran in the Business Process Outsourcing industry with over 12 years of experience. His primary focus is on empowering businesses to reach their full potential through efficient and strategic outsourcing. Richard's client-centric approach has been the driving force behind our company's operations, ensuring we prioritize the needs of our clients and deliver outstanding services tailored to their growth objectives. His expertise has fostered an ethos within our company that emphasizes client success above all else.
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- CARD 2 -->
+                    <div class="team_card">
+                        <div class="team_picture_2"></div>
+                        <div class="team_text">
+                            <div class="team_position">
+                                <p>IT Operations Manager</p>
+                            </div>
+                            <div class="team_name">
+                                <p>Michael Ryan Dinio</p>
+                            </div>
+                        </div>
+                        <div class="team_content">
+                            <p>
+                                Michael Ryan Dinio is an experienced IT Operations Manager with expertise in managing complex IT systems, optimizing technology infrastructure, and delivering reliable and cost-effective solutions. He has extensive experience in managing and maintaining servers, network devices, desktop PCs, and other IT equipment, as well as in designing and implementing network infrastructure and managing IT projects.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <!-- COLUMN 2 -->
+                <div class="team_col">
+
+                    <!-- CARD 3 -->
+                    <div class="team_card">
+                        <div class="team_picture_3"></div>
+                        <div class="team_text">
+                            <div class="team_position">
+                                <p>President</p>
+                            </div>
+                            <div class="team_name">
+                                <p>Anna Grace Portugal</p>
+                            </div>
+                        </div>
+                        <div class="team_content">
+                            <p>
+                                Anna is a highly skilled Certified Public Accountant (CPA) and Board of Accountancy (BOA) Accredited CPA in Commerce & Industry and Public Practice. Her credentials are a testament to her extensive knowledge in accounting, taxation, auditing, and financial management. With her excellent academic background and years of experience, Anna has a strong track record of providing outstanding financial advice and managing all aspects of an organization's finances.
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- CARD 4 -->
+                    <div class="team_card">
+                        <div class="team_picture_4"></div>
+                        <div class="team_text">
+                            <div class="team_position">
+                                <p>Senior Operations Manager</p>
+                            </div>
+                            <div class="team_name">
+                                <p>Moppet Castillo</p>
+                            </div>
+                        </div>
+                        <div class="team_content">
+                            <p>
+                                Moppet is a highly skilled operations professional with 20 years of experience managing and optimizing complex processes across various projects. As our savvy site director, she excels in handling all types of campaigns and has a proven track record of achieving operational excellence. Outside of work, Moppet spends her time at church, giving motivation and spiritual support to single parents like herself.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <!-- COLUMN 3 -->
+                <div class="team_col">
+
+                    <!-- CARD 5 -->
+                    <div class="team_card">
+                        <div class="team_picture_5"></div>
+                        <div class="team_text">
+                            <div class="team_position">
+                                <p>Senior Operations Manager</p>
+                            </div>
+                            <div class="team_name">
+                                <p>Moppet Castillo</p>
+                            </div>
+                        </div>
+                        <div class="team_content">
+                            <p>
+                                Moppet is a highly skilled operations professional with 20 years of experience managing and optimizing complex processes across various projects. As our savvy site director, she excels in handling all types of campaigns and has a proven track record of achieving operational excellence. Outside of work, Moppet spends her time at church, giving motivation and spiritual support to single parents like herself.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+</div>
 </div>
 
 <?php get_footer(); ?>
